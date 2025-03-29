46689a7439cc1ccb199f5edac854d03429dc3a6d
abu0127 <sr20010127@gmail.com>
2025-03-29 22:27:38 +0300

    add

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 
-const home_foto = document.querySelector('.home-foto');
-const greeting = document.querySelector('.greeting');
-const about = document.querySelector('.about');
-const names = document.querySelector('.name-con');
+
+
 const sections = document.querySelectorAll('.section')
 const calendar = document.querySelector('.calendar')
 
+const about_txt ="Hurmatli aziz birodarim sizni 5 aprel kuni bo'lib otadigan nikox tuyimiz munosabati bilan soat 11:00 da yoziladigan valima dasturxonimizga taklif etamiz ~ "
+
+
 function main() {
    
+    function writeTxt(txt, target, time) {
+        let writing = document.querySelector(target);
+        writing.textContent = '';
+
+        txt.split('').forEach((character, index) => {
+            let span = document.createElement('span');
+            span.textContent = character;
+            span.style.opacity = 0;
+            span.style.transition = 'opacity 1s ease'
+            writing.appendChild(span);
+
+            setTimeout(() => span.style.opacity = 1, time * index)
+        })
+    }
+
+    writeTxt(about_txt ,'.about' ,20)
 
 
     window.addEventListener("scroll", function () {
